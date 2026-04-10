0f9a2b3c7f8f1eb107b5d198be4bd0f72337ea00
Arthur Matos Rocha <2401008@SALAS.AULAS>
2026-04-10 09:52:18 -0300

    feat: implementação de múltiplas abas com validação via window_handles

---
+++ parte2.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+options = Options()
+options.add_argument("--headless=new")
+options.add_argument("--window-size=1920,1080")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=options
+)
+
+driver.implicitly_wait(10)
+
+driver.get("https://the-internet.herokuapp.com/dropdown")
+
+print("Abas abertas:", len(driver.window_handles))
+
+driver.execute_script("window.open('https://the-internet.herokuapp.com/dynamic_loading/2');")
+
+while len(driver.window_handles) < 2:
+    time.sleep(0.5)
+
+print("Segunda aba aberta!")
+
+driver.switch_to.window(driver.window_handles[1])
+
+print("URL aba 1:", driver.current_url)
+
+driver.execute_script("window.open('https://pt.wikipedia.org');")
+
+while len(driver.window_handles) < 3:
+    time.sleep(0.5)
+
+print("Terceira aba aberta!")
+
+driver.switch_to.window(driver.window_handles[2])
+
+print("URL aba 2:", driver.current_url)
+
+print("Total de abas:", len(driver.window_handles))
+
+driver.quit()
